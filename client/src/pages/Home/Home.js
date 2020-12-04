e0d29bebcb4787e81fbddaa9853288368e000ede
Ryan Garza <ryangarza@live.com>
2020-12-04 12:44:15 -0600

    switch component centered on home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,15 +3,40 @@ import Switch from '../../components/Switch/Switch';
 import MyChategories from '../../components/MyChategories/MyChategories';
 import MyPosts from '../../components/MyPosts/MyPosts';
 
+import OtherUserNameCard from "../../components/OtherUserNameCard/OtherUserNameCard";
+
+
+import "./Home.css";
+
 function Home() {
 
     return (
-        <>
-        <h1>This is the HOME page</h1>
+        <div>
 
-        <Switch left="off" right="on" LDisplay={<MyChategories/>} RDisplay={<MyPosts/>}/>
-          
-        </>
+            <div className="container MuiContainer-maxWidthSm">
+
+                <OtherUserNameCard />
+
+            </div>
+
+            <div className="container">
+
+                <div className="row">
+
+                    <div className="col AlignCenter">
+
+                        <div className="AlignCenter">
+                        <Switch left={<h6>MyChategories</h6>} right={<h6>My Posts</h6>} LDisplay={<MyChategories />} RDisplay={<MyPosts />} />
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+            
+        </div>
     )
 }
 
