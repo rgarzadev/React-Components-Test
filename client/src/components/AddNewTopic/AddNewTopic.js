a7caff8212816e6499b83478ea49cfc7fa57ae71
Ryan Garza <ryangarza@live.com>
2020-12-05 13:48:39 -0600

    selected chategory page with add to my button

---
+++ client/src/components/AddNewTopic/AddNewTopic.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { Modal } from 'react-bootstrap';
+import {Button, TextField} from '@material-ui/core';
+
+import "./AddNewTopic.css";
+
+const AddNewTopic = (props) => {
+
+    // const [chategory, setChategory] = useState("");
+
+    // const onChategoryFieldChange = (event) => {
+    //     setChategory(event.target.value);
+    // }
+
+    const [topic, setTopic] = useState("");
+
+    const onTopicFieldChange = (event) => {
+        setTopic(event.target.value);
+    }
+
+    const onButtonClick = (event) => {
+        console.log("make api call");
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body>
+                {/* <div className="Name">
+                    <TextField fullWidth="true" label="New Chategory Name" variant="outlined" onChange={()=>onChategoryFieldChange()}/><br/>
+                </div> */}
+
+                <div className="Topic">
+                    <TextField fullWidth="true" label="New Chategory Topic" variant="outlined" onChange={()=>onTopicFieldChange()}/><br/>
+                </div>
+
+                <div className="AddNewButton">
+                    <Button variant="contained" color="primary" onClick={()=>onButtonClick()} >Add NEW Topic</Button>
+                </div>
+
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default AddNewTopic
--- client/src/pages/SelectedChategory/SelectedChategory.js
+++ client/src/pages/SelectedChategory/SelectedChategory.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Button } from '@material-ui/core';
+
 import ChategoryTopicsList from "../../components/ChategoryTopicsList/ChategoryTopicsList";
 import ChategoryNameCard from "../../components/ChategoryNameCard/ChategoryNameCard";
+import AddNewTopic from "../../components/AddNewTopic/AddNewTopic";
 
 
 import "./SelectedChategory.css";
+
+
+const onButtonClick = (event) => {
+    console.log("make api call");
+}
 
 function SelectedChategory() {
     return (
@@ -17,17 +25,25 @@ function SelectedChategory() {
 
                     <div className="col AlignCenter">
 
-
-
                         <div className="container MuiContainer-maxWidthSm">
                             <h5 className="PageTitle">~ Selected Chategory ~</h5>
                         </div>
 
                         <ChategoryNameCard />
 
-                        <ChategoryTopicsList />
+                        <br/>
 
+                        <div className="AddToMyChategories">
+                            <Button variant="contained" color="primary" onClick={() => onButtonClick()} >Add to MY Chategories</Button>
+                        </div>
 
+                        <br/>
+
+                        <ChategoryTopicsList/>
+
+                        <br/>
+
+                        <AddNewTopic/>
 
                     </div>
 
