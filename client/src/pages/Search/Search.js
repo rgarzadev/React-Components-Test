178177c43d26f76655d7a927ee10fa1d0acc5376
Ryan Garza <ryangarza@live.com>
2020-12-04 22:53:53 -0600

    all elements showing and centered on mobile search page

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -2,6 +2,9 @@ import { makeStyles, Container } from '@material-ui/core';
 import React, { useState } from 'react';
 import { TextField, Switch, Button } from '@material-ui/core';
 import AddNewChategory from '../../components/AddNewChategory/AddNewChategory';
+import NewestChategories from '../../components/NewestChategories/NewestChategories';
+import SearchResults from '../../components/SearchResults/SearchResults';
+
 
 function Search() {
 
@@ -40,7 +43,7 @@ function Search() {
 
                         {"Newest Chategories"} <Switch checked={toggleState} onChange={() => setToggleState(!toggleState)} /> {"Search Results"}
 
-                        <div>{toggleState ? "Yay I'm on" : "I'm off"}</div>
+                        <div>{toggleState ? <NewestChategories/> : <SearchResults/>}</div>
 
                         <Button variant="contained" color="primary" onClick={() => setModalShow(true)}>Add a NEW Chategory</Button>
 
