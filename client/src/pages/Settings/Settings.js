46e695ae1984f006486c87b99b54a596d2ce7b88
David Anderson <david.anderson1993@gmail.com>
2020-12-05 15:22:30 -0600

    updating the settings slash profile page ready to take in data and save button has class name ready for posting to db

--- client/src/pages/Settings/Settings.js
+++ client/src/pages/Settings/Settings.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
 
 import "./Settings.css";
 
 
 function Settings() {
     return (
-        <div className= "OtherUserNamePlate" maxWidth="sm">
 
-            <br></br>
+        <Container>
+            <div className= "OtherUserNamePlate" maxWidth="sm">
 
-            <h5> Edit Profile </h5>
+                    <br></br>
 
-            <br></br>
+                <h5> Edit Profile </h5>
 
-        </div>
+                    <br></br>
 
+                <div className="MyUserImage">
+
+                </div>
+
+                    <br></br>
+
+                <div>
+
+                    <input className="usernameTextInput" placeholder="Username"></input>
+
+                </div>
+
+                    <br></br>
+
+                <div>
+
+                <input className="aboutMeTextInput" placeholder="Tell us about yourself!"></input>
+
+                </div>
+
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <SaveIcon className="saveBtn" fontSize="large"/>
+                </IconButton>
+
+            </div>
+        </Container>
     )
 }
 
